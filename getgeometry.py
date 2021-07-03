85d708f314191f049cd09cdfe2838c0e004f2178
takamotobiz <takamotobiz@users.noreply.github.com>
2021-07-04 07:55:08 +0900

    main

--- getgeometry.py
+++ getgeometry.py
@@ -1,4 +1,4 @@
-outputpath = '/Users/takamotokeiji/github/takamotobiz/getqgisgeometry/output.txt'
+outputpath = '__FULLPATH__/output.txt'
 with open(outputpath, mode='w') as f:
     #get active layer
     layer = iface.activeLayer()
@@ -8,5 +8,5 @@ with open(outputpath, mode='w') as f:
         #get geometry
         f.write(feature.geometry().asWkt())
         #get one of property(Attribute)
-        print(feature['name_ja'])
+        #print(feature['name_ja'])
         
